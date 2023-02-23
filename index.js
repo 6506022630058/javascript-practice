717b49e9ca334e55f7f7a6bdbdb781ad9152750c
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-02-23 15:43:39 +0700

    jj

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// The code was missing a closing curly brace '}' at the end. The code should look like this:
+console.log('Goodbye world');
+let x = 2;
+x = 10;
+console.log('Goodbye world');
+console.log(x);
+console.log('Goodbye world');
+
